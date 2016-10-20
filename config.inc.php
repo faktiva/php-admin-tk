dcc7757c3af51f8ae62cf783be621e8456a7e1ec
Emiliano 'AlberT' Gabrielli <albert@faktiva.com>
2016-10-20 12:43:54 +0200

    Should fix use of BASE_URI

--- config.inc.php
+++ config.inc.php
@@ -7,13 +7,12 @@
 
 ini_set('display_errors', 1);
 ini_set('log_errors', 1);
+umask(0);
+
+require_once __DIR__.'/vendor/autoload.php';
 
 define('BASE_DIR', __DIR__.'/httpdocs');
 define('BASE_URI', rtrim(dirname($_SERVER['PHP_SELF']), '/'));
 
 define('TOOLS_DIR', BASE_DIR.'/tools');
 define('TOOLS_URI', BASE_URI.'/tools');
-
-umask(0);
-
-require_once __DIR__.'/vendor/autoload.php';
