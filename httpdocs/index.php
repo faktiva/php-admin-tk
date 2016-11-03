6131a59c0bb3da7bfd2fac00e8ab354c7a98458e
Emiliano 'AlberT' Gabrielli <albert@faktiva.com>
2016-11-03 17:05:56 +0100

    Fix CS

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -1,8 +1,21 @@
 <?php
+
 /*
- * Admin Tools Dashboard
+ * This file is part of the Faktiva "PHP Admin ToolKit".
  *
- * @author "Emiliano Gabrielli" <albert@faktiva.com>
+ * (c) Faktiva (http://faktiva.com)
+ *
+ * NOTICE OF LICENSE
+ * This source file is subject to the CC BY-SA 4.0 license that is
+ * available at the URL https://creativecommons.org/licenses/by-sa/4.0/
+ *
+ * DISCLAIMER
+ * This code is provided as is without any warranty.
+ * No promise of being safe or secure
+ *
+ * @author   Emiliano 'AlberT' Gabrielli <albert@faktiva.com>
+ * @license  https://creativecommons.org/licenses/by-sa/4.0/  CC-BY-SA-4.0
+ * @source   https://github.com/faktiva/php-admin-tk
  */
 
 require __DIR__.'/../config.inc.php';
